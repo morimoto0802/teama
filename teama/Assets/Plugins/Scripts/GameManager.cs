e5310387c2ab45ff5aa2f3718f94bdb0f228da7b
morimoto0802 <109467308+morimoto0802@users.noreply.github.com>
2022-09-18 22:47:26 +0900

    2022/9/18/22:44
    Assets>PluginsにMiniJsonとJsonNodeというファイルを作った。GameManagerを作った。Sample.jsonというJson形式のノーツ情報（仮）を保存した。

---
+++ teama/Assets/Plugins/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] string FilePath;
+
+    [SerializeField] Button Play;
+    [SerializeField] Button SetChart;
+
+    [SerializeField] GameObject aka;
+    [SerializeField] GameObject ki;
+    [SerializeField] GameObject ao;
+    [SerializeField] GameObject midori;
+    [SerializeField] GameObject murasaki;
+
+    [SerializeField] Transform SpawnPointaka;
+    [SerializeField] Transform SpawnPointki;
+    [SerializeField] Transform SpawnPointao;
+    [SerializeField] Transform SpawnPointmidori;
+    [SerializeField] Transform SpawnPointmurasaki;
+    [SerializeField] Transform BeatPointaka;
+    [SerializeField] Transform BeatPointki;
+    [SerializeField] Transform BeatPointao;
+    [SerializeField] Transform BeatPointmidori;
+    [SerializeField] Transform BeatPointmurasaki;
+
+    string Title;
+    int BPM;
+    List<GameObject> Notes;
+
+    void OneEnable()
+    {
+        Play.onClick
+            .AsObservable()
+            .Sbscribe(_ => Play());
+
+        SetChart.onClick
+            .AsObservable()
+            .Sunscribe(_ => loadChart());
+    }
+
+    void loadChart()
+    {
+        Notes = new List<GameObject>();
+
+        string jsonText = Resources.Load<TextAsset>(FilePath).ToString();
+
+        JsonNode json = JsonNode.Parse(jsonText);
+        Title = json["title"].Get<string>();
+        BPM = int.Parse(json["bpm"].Get<string>());
+
+        foreach (var note in json["notes"])
+        {
+            string type = note["type"].Get<string>();
+            float timing = float.Parse(note["timing"].Get<string>());
+
+            GameObject Note;
+            if (type == "aka")
+            {
+                Note = Instantiate(aka, SpawnPointaka.position, Quaternion.identity);
+            }
+            else if (type == "ki")
+            {
+                Note = Instantiate(ki, SpawnPointki.position, Quaternion.identity);
+            }
+            else if (type == "ao")
+            {
+                Note = Instantiate(ao, SpawnPointao.position, Quaternion.identity);
+            }
+            else if (type == "midori")
+            {
+                Note = Instantiate(midori, SpawnPointmidori.position, Quaternion.identity);
+            }
+            else if (type == "murasaki")
+            {
+                Note = Instantiate(murasaki, SpawnPointmurasaki.position, Quaternion.identity);
+            }
+
+            Notes.Add(Note);
+        }
+    }
+
+    void play()
+    {
+        Debug.Log("Game Start!");
+    }
+}
+
