f5482f2c3d471dc704ecfbaddecf6a56a983fa63
morimoto0802 <109467308+morimoto0802@users.noreply.github.com>
2022-12-09 12:45:11 +0900

    Revert "2022/11/12"
    This reverts commit 94a988ab94abb22d0a1a0a255378e2ddd6851eaf.

--- teama/Assets/Plugins/Scripts/GameManager.cs
+++ teama/Assets/Plugins/Scripts/GameManager.cs
@@ -14,7 +14,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] Button Play;
     [SerializeField] Button SetChart;
 
-
     [SerializeField] GameObject aka;
     [SerializeField] GameObject ki;
     [SerializeField] GameObject ao;
@@ -43,8 +42,6 @@ public class GameManager : MonoBehaviour
     float CheckRange;
     float BeatRange;
     List<float> NoteTimings;
-
-
 
     string Title;
     int BPM;
@@ -94,7 +91,6 @@ public class GameManager : MonoBehaviour
                 Notes[GoIndex].GetComponent<NoteController>().go(Distance, During);
                 GoIndex++;
             });
-
 
         this.UpdateAsObservable()
             .Where(_ => isPlaying)
@@ -191,9 +187,8 @@ public class GameManager : MonoBehaviour
             Debug.Log("notekazu" + Notes.Count);
             NoteTimings.Add(timing);
         }
-        Debug.Log("notekazu" + Notes.Count);
+        Debug.Log("notekazu"+ Notes.Count) ;
     }
-
 
     void play()
     {
@@ -214,7 +209,7 @@ public class GameManager : MonoBehaviour
             if (NoteTimings[i] > 0)
             {
                 float diff = Math.Abs(NoteTimings[i] - timing);
-                if (minDiff == -1 || minDiff > diff)
+                if(minDiff == -1 || minDiff > diff)
                 {
                     minDiff = diff;
                     minDiffIndex = i;
@@ -222,9 +217,9 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        if (minDiff != -1 & minDiff < CheckRange)
+        if(minDiff != -1 & minDiff < CheckRange)
         {
-            if (minDiff < BeatRange & Notes[minDiffIndex].GetComponent<NoteController>().getType() == type)
+            if(minDiff < BeatRange & Notes[minDiffIndex].GetComponent<NoteController>().getType() == type)
             {
                 NoteTimings[minDiffIndex] = -1;
                 Notes[minDiffIndex].SetActive(false);
